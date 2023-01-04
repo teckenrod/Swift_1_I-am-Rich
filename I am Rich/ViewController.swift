f4f4c83f527d0fd3d873120c7efdb63ad9d02e0d
Trey Eckenrod <teckenrod@uiowa.edu>
2023-01-04 17:22:49 -0600

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Trey Eckenrod on 1/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
